8b3b9905dae15e31f080142343ba2bb80e6b4b68
kethavath raju <rajukethavath4778@gmail.com>
2025-03-03 15:57:04 +0530

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,27 @@ import React from "react";
 import NavBar from "./components/NavBar";
 import Banner from "./components/Banner";
 import MovieCard from "./components/MovieCard";
-
+import WatchList from "./components/WatchList";
+import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 const App = () => {
   return (
     <>
       <NavBar />
-      <Banner />
-      <MovieCard />
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <div>
+                <Banner />
+                <MovieCard />
+              </div>
+            }
+          />
+          <Route path="/watchlist" element={<WatchList />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
