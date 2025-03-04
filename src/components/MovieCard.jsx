8b57126bcbf3e91fcdc1b98e93a373a5eda10e23
kethavath raju <rajukethavath4778@gmail.com>
2025-03-04 14:45:17 +0530

    axios

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,6 +1,14 @@
 import React from "react";
+import axios from "axios";
 
 const MovieCard = () => {
+  axios
+    .get(
+      "https://api.themoviedb.org/3/trending/movie/day?api_key=9982b8ceb05b6fbfc1744f1b8e50f2fd"
+    )
+    .then((response) => {
+      console.log(response.data.results);
+    });
   return (
     <div>
       <div className="text-black text-xl mb-8 font-bold  text-center">
@@ -13,14 +21,11 @@ const MovieCard = () => {
             backgroundImage:
               "url(https://therunngun.com/wp-content/uploads/2023/11/John-Wick-Composite-Low-Rez-768x1152.jpg)",
           }}
-        ></div>
-        <div
-          className="w-[160px] h-[50vh] md:h-[50vh] bg-center bg-contain hover:scale-110 duration 300 flex items-end  rounded-xl m-2 "
-          style={{
-            backgroundImage:
-              "url(https://therunngun.com/wp-content/uploads/2023/11/John-Wick-Composite-Low-Rez-768x1152.jpg)",
-          }}
-        ></div>
+        >
+          <div className="text-white font-bold text-center w-full bg-gray-900 bg-opacity-50 p-2 rounded-b-xl">
+            John Wick
+          </div>
+        </div>
       </div>
     </div>
   );
