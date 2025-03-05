8bffc5e30a1fe43034937955f54f4873bb1918e8
kethavath raju <rajukethavath4778@gmail.com>
2025-03-05 17:32:20 +0530

    Movies Page

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,31 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
 const MovieCard = () => {
-  axios
-    .get(
-      "https://api.themoviedb.org/3/trending/movie/day?api_key=9982b8ceb05b6fbfc1744f1b8e50f2fd"
-    )
-    .then((response) => {
-      console.log(response.data.results);
-    });
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    (function () {
+      axios
+        .get(
+          "https://api.themoviedb.org/3/trending/movie/day?api_key=9982b8ceb05b6fbfc1744f1b8e50f2fd"
+        )
+        .then((response) => {
+          //response or res  adina pettukovachu just store on that anthee
+          //console.log(response.data.results);
+          setMovies(response.data.results);
+        });
+    })();
+
+    console.log("Test");
+  }, []);
+  console.log(movies);
+
   return (
     <div>
       <div className="text-black text-xl mb-8 font-bold  text-center">
         Trending Movies
       </div>
-      <div className="flex ">
-        <div
-          className="w-[160px] h-[50vh] md:h-[50vh] bg-center bg-contain hover:scale-110 duration 300 flex items-end  rounded-xl m-2 "
-          style={{
-            backgroundImage:
-              "url(https://therunngun.com/wp-content/uploads/2023/11/John-Wick-Composite-Low-Rez-768x1152.jpg)",
-          }}
-        >
-          <div className="text-white font-bold text-center w-full bg-gray-900 bg-opacity-50 p-2 rounded-b-xl">
-            John Wick
-          </div>
-        </div>
+      <div className="flex flex-wrap ">
+        {movies.map((movie) => {
+          return (
+            <div
+              className="w-[150px] h-[50vh] md:h-[50vh] bg-center bg-contain hover:scale-110 duration 300 flex items-end  rounded-xl m-2 "
+              style={{
+                backgroundImage: `url(https://image.tmdb.org/tp/original/t/p/w500/${movie.poster_path})`,
+              }}
+            >
+              <div className="text-white font-bold text-center w-full bg-gray-900 bg-opacity-50 p-2 rounded-b-xl">
+                {movie.title}
+              </div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
